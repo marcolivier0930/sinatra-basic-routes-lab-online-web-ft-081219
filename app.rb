44b74acd8980778f7813a289c5747285b0d8d1dd
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-11 19:59:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Marc"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Jacmel"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Under the Sun"
+  end
+  
 end
