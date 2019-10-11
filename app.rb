99c8014a3bd97d4e9d2c6c05e9e3e42dab818eca
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-11 19:58:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
-    "My name is _"
+    "My name is Marc"
   end
   
   get '/hometown' do 
-    "My hometown is _"
+    "My hometown is Jacmel"
   end
   
   get '/favorite-song' do 
-    "My favorite-song is _"
+    "My favorite-song is Under the Sun"
   end
   
 end
